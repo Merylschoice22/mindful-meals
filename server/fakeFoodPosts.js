013dafad607b600ee7312550f7098e5cf13e6bc9
Enia Munteanu <75390874+Moonia9@users.noreply.github.com>
2021-07-27 20:49:23 +0200

    added a test array for FakeFoodPosts

---
+++ server/fakeFoodPosts.js
@@ -0,0 +1,10 @@
+const foodPosts = [
+  {
+    id: 1,
+    title: "Pizza",
+    location: "Gracia",
+    phone: "623 479 840",
+    description:
+      "I have a leftover cheese pizza from a party. Enough for two people",
+  },
+];
