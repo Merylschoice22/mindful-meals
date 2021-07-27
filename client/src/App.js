f8dbaace4726f4eb61f677c2d369100f5fa72b0c
Enia Munteanu <75390874+Moonia9@users.noreply.github.com>
2021-07-27 18:04:41 +0200

    updated package json react-router-dom

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,13 @@
+import React from "react";
 import "./App.css";
+import { BrowserRouter, Switch, Route, Link } from "react-router-dom";
 
 function App() {
-  return <div className="App">Mindful Meals</div>;
+  return (
+    <BrowserRouter>
+      <div className="App">Mindful Meals</div>
+    </BrowserRouter>
+  ); 
 }
 
 export default App;
