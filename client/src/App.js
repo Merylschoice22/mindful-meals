18555f8dbc3d64a1cae4ee7f04e832a4fe79ec37
Enia Munteanu <75390874+Moonia9@users.noreply.github.com>
2021-07-27 20:29:54 +0200

    added FoodPostForm basic skeleton

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,15 @@
 import React from "react";
 import "./App.css";
 import { BrowserRouter, Switch, Route, Link } from "react-router-dom";
+//import FoodPostForm from "./FoodPostForm";
 
 function App() {
   return (
     <BrowserRouter>
       <div className="App">Mindful Meals</div>
+      {/* <FoodPostForm /> */}
     </BrowserRouter>
-  ); 
+  );
 }
 
 export default App;
