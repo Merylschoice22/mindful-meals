7bf0dbfa7eef79d4670204081c71f8cccb91f17e
Enia Munteanu <75390874+Moonia9@users.noreply.github.com>
2021-07-28 22:00:23 +0200

    connected FoodCard with App.js

--- client/src/App.js
+++ client/src/App.js
@@ -2,12 +2,17 @@ import React from "react";
 import "./App.css";
 import { BrowserRouter, Switch, Route, Link } from "react-router-dom";
 //import FoodPostForm from "./FoodPostForm";
+import FoodCard from "./FoodCard";
 
 function App() {
   return (
     <BrowserRouter>
       <div className="App">Mindful Meals</div>
       {/* <FoodPostForm /> */}
+      <div className="wrapper">
+        <FoodCard />
+        {/* Repeat with as many cards */}
+      </div>
     </BrowserRouter>
   );
 }
