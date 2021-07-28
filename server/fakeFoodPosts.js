aaca5afa09c8f6f87fb520206b97941cb2a35353
merylschoice22 <kekrieg.digital@gmail.com>
2021-07-28 19:23:54 +0200

    Update fakefoodposts

--- server/fakeFoodPosts.js
+++ server/fakeFoodPosts.js
@@ -7,7 +7,6 @@ const foodPosts = [
     description:
       "I have a leftover cheese pizza from a party. Enough for two people",
   },
-
   {
     id: 1,
     title: "Carrot Cake",
@@ -42,5 +41,46 @@ const foodPosts = [
     phone: "637 759 903",
     description:
       "I would like to give away a freshly made salad. Enough for two servings",
+  },
+  ,
+  {
+    id: 5,
+    title: "Leftover lasagna 3 pieces",
+    location: "Born, next to Arc de Triomf",
+    phone: "692 880 548",
+    description:
+      "I made lasagna and have 3 pieces leftover that I won't be able to finish. It is with meat, onions, and zucchini and bechamel cheese",
+  },
+  {
+    id: 6,
+    title: "AMAZING vegetable pasta",
+    location: "Eixample Dreta, Arago, con Bailen",
+    phone: "655 734 400",
+    description:
+      "Pasta with mixed vegetables and herbs. Bring your own tupperware container to put it in please!",
+  },
+  {
+    id: 7,
+    title: "Grilled Salmon and mashed potatoes",
+    location: "Compte Urgell & Floridablanca",
+    phone: "663 121 2198",
+    description:
+      "I have half of a grilled salmon piece and some mashed potatoes if anyone wants it",
+  },
+  {
+    id: 8,
+    title: "Extra chips and some drinks",
+    location: "Barceloneta",
+    phone: "623 374 7444",
+    description:
+      "We had a party and there are leftover snacks. Everything has been opened except one bag of nuts. It's a few bags chips, some Coke, Coke Zero, and a Fanta Limon.",
+  },
+  {
+    id: 9,
+    title: "Vegetables",
+    location: "Eixample - Villaroel / Valencia",
+    phone: "646 885 5215",
+    description:
+      "Extra vegetables from the fridge that I won't be able to finish before they expire. 2 tomatoes, 1 onion, 4 potatoes, and half a green pepper",
   },
 ];
