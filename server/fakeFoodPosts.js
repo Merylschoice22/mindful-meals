7b0fb54e730aecbe7a30eb963b677be10e816369
Enia Munteanu <75390874+Moonia9@users.noreply.github.com>
2021-07-28 19:19:42 +0200

    added more fake food posts

--- server/fakeFoodPosts.js
+++ server/fakeFoodPosts.js
@@ -1,10 +1,46 @@
 const foodPosts = [
   {
-    id: 1,
+    id: 0,
     title: "Pizza",
-    location: "Gracia",
+    location: "Gracia, Carrer de Rabassa no, 67",
     phone: "623 479 840",
     description:
       "I have a leftover cheese pizza from a party. Enough for two people",
+  },
+
+  {
+    id: 1,
+    title: "Carrot Cake",
+    location: "Sant Andreu de Palomar, Carrer Malats, no 46",
+    phone: "623 474 908",
+    description:
+      "I would like to give away a vegan carrot cake that I baked this morning. Enough for four people :)",
+  },
+
+  {
+    id: 2,
+    title: "Cupcakes",
+    location: "Carmel, Carrer de Ramon Rocafull, no 91",
+    phone: "623 459 004",
+    description:
+      "I would like to give away some blueberry muffins. There are about 20 waiting to be shared",
+  },
+
+  {
+    id: 3,
+    title: "Chickpea Curry",
+    location: "Sant Marti",
+    phone: "637 908 625",
+    description:
+      "I would like to give away some blueberry muffins. There are about 20 waiting to be shared",
+  },
+
+  {
+    id: 4,
+    title: "Primavera Pasta Salad",
+    location: "L'Eixample",
+    phone: "637 759 903",
+    description:
+      "I would like to give away a freshly made salad. Enough for two servings",
   },
 ];
