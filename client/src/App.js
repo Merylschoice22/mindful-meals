96778594de78181b3be673318bc70363cd6cfe7e
merylschoice22 <kekrieg.digital@gmail.com>
2021-07-29 10:50:20 +0200

    Update App.js structure and implement multiple pages

--- client/src/App.js
+++ client/src/App.js
@@ -2,10 +2,27 @@ import React from "react";
 import "./App.css";
 import { BrowserRouter, Switch, Route, Link } from "react-router-dom";
 
-function App() {
+const App = () => {
   return (
     <BrowserRouter>
-      <div className="App">Mindful Meals</div>
+      <div className="App">
+        <header>
+          <div className="header-text">
+            <h4>Mindful Meals</h4>
+            <h6>Tagline</h6>
+          </div>
+          <Navbar />
+        </header>
+        <Switch>
+          <Route path="/" component={FoodFeed} />
+          <Route path="/" component={} />
+          <Route path="/" component={} />
+          <Route path="/" component={} />
+        </Switch>
+        <footer>
+          <h6>Footer text</h6>
+        </footer>
+      </div>
     </BrowserRouter>
   ); 
 }
