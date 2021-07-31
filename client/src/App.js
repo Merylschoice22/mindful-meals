65748b4b4643d5b0bb7de10ff762dffe22dddb3b
codemaoz90 <codemaoz90@gmail.com>
2021-07-31 16:57:21 +0200

    Login and Register styling

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,22 @@
 import React from "react";
-import "./App.css";
+
+import { BrowserRouter, Switch, Route, Link } from "react-router-dom";
 import Login from "./components/Login";
+import Register from "./components/Register";
 
 function App() {
-  return (
-    <div className="App">
-      <Login />
-    </div>
-  );
+	return (
+		<BrowserRouter>
+			<Switch>
+				<Route path="/login">
+					<Login />
+				</Route>
+				<Route path="/register">
+					<Register />
+				</Route>
+			</Switch>
+		</BrowserRouter>
+	);
 }
 
 export default App;
