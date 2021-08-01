bd165e4bf7da58a32282af867b672832be88c4b5
Enia Munteanu <75390874+Moonia9@users.noreply.github.com>
2021-08-01 20:35:07 +0200

    defined structure Navbar.js

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -4,10 +4,36 @@ import { Link } from "react-router-dom";
 const Navbar = () => {
   return (
     <div className="navbar">
-      <Link to="/">Food Feed</Link>
-      <Link to=""></Link>
-      <Link to=""></Link>
-      <Link to=""></Link>
+      <div className="navbar-left">
+        <Link to="/">
+          <img
+            src="https://64.media.tumblr.com/865fb0d98e62d93cbd3133b1a4ec5cf0/305827feecce935f-d2/s1280x1920/b7145050ee0a932862293aab7afef8faa3096cbf.jpg"
+            alt="Welcome to Mindful Meals"
+            className="homepage-logo"
+          />
+        </Link>
+      </div>
+      <div className="homepage-input-search">
+        <input type="text" placeholder="Search" />
+      </div>
+
+      <section>
+        <div className="menu-bar"></div>
+      </section>
+      <div className="navbar-right">
+        <section>
+          <div className="navbar-links">
+            <div className="content">
+              <Link to="" className="navbar-link">
+                My Food Posts
+              </Link>
+              <Link to="" className="navbar-link">
+                My Food
+              </Link>
+            </div>
+          </div>
+        </section>
+      </div>
     </div>
   );
 };
