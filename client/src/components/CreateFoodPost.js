ab81d7a43cb7e45c69676fd4dec46aeffcbaa9cd
Enia Munteanu <75390874+Moonia9@users.noreply.github.com>
2021-08-01 23:24:06 +0200

    linked css to the files

--- client/src/components/CreateFoodPost.js
+++ client/src/components/CreateFoodPost.js
@@ -2,6 +2,7 @@ import React from "react";
 import { useState } from "react";
 import PopupInput from "./components/PopupInput";
 import FoodPostForm from "./components/FoodPostForm";
+import "./components/CreateFoodPost.css";
 
 function CreateFoodPost(props) {
   const [inputPopup, setInputPopup] = useState(false);
