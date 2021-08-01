fe475fb9fed8c06451a379689d02ba3043017d3f
Enia Munteanu <75390874+Moonia9@users.noreply.github.com>
2021-08-01 22:26:46 +0200

    added the structure of the information

--- client/src/components/ReservedPopup.js
+++ client/src/components/ReservedPopup.js
@@ -1,7 +1,48 @@
 import React from "react";
 
-function ReservedPopup() {
-  return <div></div>;
+function ReservedPopup(props) {
+  return props.trigger ? (
+    <div className="popup">
+      <div className="inner-popup">
+        <div className="reserve-post-information">
+          <h3>We see that you are interested in this treat :)</h3>
+          <br></br>
+          <div className="command-intro">
+            <p>
+              You are one step away to reserve the food ! The above information
+              belongs to the Food Giver and we would like to ask you to contact
+              the person using the following :
+            </p>
+            <br></br>
+          </div>
+          <div className="personal-details-post-owner">
+            <div>
+              <p className="location-food-giver">
+                Location is : Gracia, Carrer de Rabassa
+              </p>
+              <br></br>
+              <p className="phone-food-giver">
+                The phone number is : 623 479 840
+              </p>
+            </div>
+          </div>
+          <br></br>
+          <p>Click the button down below if everything sounds good for you </p>
+          <br></br>
+          <button
+            className="reserved-btn"
+            onClick={() => {
+              props.setTrigger(false);
+            }}
+          >
+            Reserved
+          </button>
+          {props.children}
+        </div>
+      </div>
+    </div>
+  ) : (
+    ""
+  );
 }
-
 export default ReservedPopup;
