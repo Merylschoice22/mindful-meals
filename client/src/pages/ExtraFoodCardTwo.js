6a6771bf84607bd0df7820012d009e8ccdd0332a
Enia Munteanu <75390874+Moonia9@users.noreply.github.com>
2021-08-01 22:51:06 +0200

    updated food post card two

--- client/src/pages/ExtraFoodCardTwo.js
+++ client/src/pages/ExtraFoodCardTwo.js
@@ -13,24 +13,24 @@ function ExtraFoodCardTwo() {
           <div className="post-top-left">
             <img
               className="post-profile-img"
-              src="https://cdn.dribbble.com/users/77224/screenshots/5601119/reading_dribbble_03.png?compress=1&resize=800x600"
+              src="https://cdn.dribbble.com/users/77224/screenshots/5422161/plant_dribbble3.png?compress=1&resize=800x600"
               alt=""
             ></img>
-            <span className="post-username">Frida Amor</span>
+            <span className="post-username">Mario Totti</span>
           </div>
           <div className="post-top-right">
-            <span className="post-location">Gracia</span>
+            <span className="post-location">La Sagrera</span>
           </div>
         </div>
         <div className="post-center">
           <span className="post-description-title">Hey there!</span>
           <p className="post-description">
-            I would like to give away a vegan carrot cake that I baked this
-            morning. Enough for four people :)
+            I would like to give away some blueberry muffins. There are about 20
+            waiting to be shared xD
           </p>
           <img
             className="post-image"
-            src="https://img.delicious.com.au/Kl8Gm8gM/del/2017/05/spiced-carrot-cake-with-labneh-icing-46402-2.jpg"
+            src="https://d1e3z2jco40k3v.cloudfront.net/-/media/mccormick-us/recipes/mccormick/b/2000/blueberry-muffins.jpg?rev=213d90493899477b8b156781c8aa06d0&vd=20200628T055650Z&hash=37CA7866D18FBD195C495824DE373751"
           ></img>
         </div>
         <div className="going-bottom">
