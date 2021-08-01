eec9f703497808e102cae50773f44276a698c8c9
Enia Munteanu <75390874+Moonia9@users.noreply.github.com>
2021-08-01 22:47:06 +0200

    adding a second duplicate of the food card post

---
+++ client/src/pages/ExtraFoodCardTwo.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+function ExtraFoodCardTwo() {
+  return <div></div>;
+}
+
+export default ExtraFoodCardTwo;
--- client/src/pages/FoodFeed.js
+++ client/src/pages/FoodFeed.js
@@ -1,11 +1,13 @@
 import React from "react";
 import CreateFoodPost from "../components/CreateFoodPost.js";
+import ExtraFoodCardOne from "./ExtraFoodCardOne.js";
 
 const FoodFeed = () => {
   return (
     <div className="food-feed">
       <h1>Mindful Meals</h1>
       <CreateFoodPost />
+      <ExtraFoodCardOne />
       {/* Component - FoodPostCard - mapped? */}
       {/* Component - LoadMore button */}
     </div>
