34803ce10e7b1935195118f794525ff779cf7143
Enia Munteanu <75390874+Moonia9@users.noreply.github.com>
2021-08-01 22:23:33 +0200

    added structure to FoodPostCard.js

--- client/src/components/FoodPostCard.js
+++ client/src/components/FoodPostCard.js
@@ -1,7 +1,40 @@
 import React from "react";
 
 function FoodPostCard() {
-  return <div></div>;
+  return (
+    <div className="post">
+      <div className="post-wrapper">
+        <div className="post-top">
+          <div className="post-top-left">
+            <img
+              className="post-profile-img"
+              src="https://cdn.dribbble.com/users/77224/screenshots/5601119/reading_dribbble_03.png?compress=1&resize=800x600"
+              alt=""
+            ></img>
+            <span className="post-username">Frida Amor</span>
+          </div>
+          <div className="post-top-right">
+            <span className="post-location">Gracia</span>
+          </div>
+        </div>
+        <div className="post-center">
+          <span className="post-description-title">Hey there!</span>
+          <p className="post-description">
+            I would like to give away a vegan carrot cake that I baked this
+            morning. Enough for four people :)
+          </p>
+          <img
+            className="post-image"
+            src="https://img.delicious.com.au/Kl8Gm8gM/del/2017/05/spiced-carrot-cake-with-labneh-icing-46402-2.jpg"
+          ></img>
+        </div>
+        <div className="going-bottom">
+          <button className="going-post-food-btn">Going!</button>
+          <br></br>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default FoodPostCard;
