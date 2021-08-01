8d5975ff254b77a549ac1f551f8f4bdf7d9ea108
Enia Munteanu <75390874+Moonia9@users.noreply.github.com>
2021-08-01 22:48:21 +0200

    adding a third duplicate of the food card post

--- client/src/pages/ExtraFoodCardTwo.js
+++ client/src/pages/ExtraFoodCardTwo.js
@@ -1,7 +1,54 @@
 import React from "react";
+import { useState } from "react";
+import "./components/FoodPostCard.css";
+import ReservedPopup from "./components/ReservedPopup";
+import "./components/ReservedPopup.css";
 
 function ExtraFoodCardTwo() {
-  return <div></div>;
+  const [buttonPopup, setButtonPopup] = useState(false);
+  return (
+    <div className="post">
+      <div className="post-wrapper">
+        <div className="post-top">
+          <div className="post-top-left">
+            <img
+              className="post-profile-img"
+              src="https://cdn.dribbble.com/users/77224/screenshots/5601119/reading_dribbble_03.png?compress=1&resize=800x600"
+              alt=""
+            ></img>
+            <span className="post-username">Frida Amor</span>
+          </div>
+          <div className="post-top-right">
+            <span className="post-location">Gracia</span>
+          </div>
+        </div>
+        <div className="post-center">
+          <span className="post-description-title">Hey there!</span>
+          <p className="post-description">
+            I would like to give away a vegan carrot cake that I baked this
+            morning. Enough for four people :)
+          </p>
+          <img
+            className="post-image"
+            src="https://img.delicious.com.au/Kl8Gm8gM/del/2017/05/spiced-carrot-cake-with-labneh-icing-46402-2.jpg"
+          ></img>
+        </div>
+        <div className="going-bottom">
+          <button
+            className="going-post-food-btn"
+            onClick={() => setButtonPopup(true)}
+          >
+            Going!
+          </button>
+          <ReservedPopup
+            trigger={buttonPopup}
+            setTrigger={setButtonPopup}
+          ></ReservedPopup>
+          <br></br>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default ExtraFoodCardTwo;
