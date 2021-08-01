45d4680cf2ff6be59f573cf0665677082dba03b5
Enia Munteanu <75390874+Moonia9@users.noreply.github.com>
2021-08-01 20:57:37 +0200

    defined structure for /post - CreateFoodPost.js

--- client/src/components/CreateFoodPost.js
+++ client/src/components/CreateFoodPost.js
@@ -1,11 +1,21 @@
-import React from 'react'
+import React from "react";
 
 function CreateFoodPost() {
   return (
-    <div>
-      
+    <div className="make-food-post-popup">
+      <div className="make-food-post-first-section">
+        <div className="first-section-input">
+          <h3>Create Post</h3>
+        </div>
+        <br></br>
+        <div className="second-section-input">
+          <input type="text" placeholder="What food are you giving away?" />
+        </div>
+        <br></br>
+        <hr />
+      </div>
     </div>
-  )
+  );
 }
 
-export default CreateFoodPost
+export default CreateFoodPost;
