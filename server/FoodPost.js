e1fcfe5421f0adfbd532472ef830a4bc09b46a9e
merylschoice22 <kekrieg.digital@gmail.com>
2021-08-07 00:34:56 +0200

    Begin POST endpoint. Define body and create basic structure

--- server/FoodPost.js
+++ server/FoodPost.js
@@ -12,10 +12,10 @@ app.use(bodyParser.json());
 const pool = new Pool(secret);
 
 //FoodFeed
-//See all food posts - username, title, barrio, description
-//See more details -
-//In order of creation
-//Where status is available
+  //See all food posts - username, title, barrio, description
+  //See more details -
+  //In order of creation
+  //Where status is available
 app.get("/", (req, res) => {
   const query =
     "SELECT u.username, p.title, p.loc_barrio, p.description, p.post_date FROM posts p INNER JOIN users u ON p.users_id = u.id order by p.post_date asc limit 5";
@@ -28,6 +28,27 @@ app.get("/", (req, res) => {
 });
 
 //Food Post Form - Create a new food post
-app.post("/new-post", (req, res) => {});
+app.post("/new-post", (req, res) => {
+  /* BODY */
+  // {
+  //     "title": "",
+  //     "loc_barrio": "",
+  //     "loc_street": "",
+  //     "phone": "",
+  //     "description": ""
+  // }
+  const post = req.body;
+
+
+  const query =;
+
+  pool
+    .query(query)
+    .then((result) => res.json(result.rows[0]))
+    .catch((error) => console.error(error));
+
+  post.title;
+  // creator.createPost("Food Post");
+});
 
 app.listen(3000, () => console.log("Server is up and running"));
