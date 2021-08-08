c4eed5b5a73a3d1e3ccba98ecf77b44502ec9068
Enia Munteanu <75390874+Moonia9@users.noreply.github.com>
2021-08-08 15:14:47 +0200

    modified structure Login.js - removed bootstrap

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -2,44 +2,17 @@ import React from "react";
 
 const Login = () => {
   return (
-    <div
-      style={{ height: "100vh", backgroundColor: "#F7EDE2" }}
-      className="d-flex justify-content-center align-items-center"
-    >
-      <form>
-        <h1 className="mb-5 text-uppercase">MindFul Meals</h1>
-        <div class="form-group">
-          <label for="exampleInputEmail1">Email address</label>
-          <input
-            type="email"
-            class="form-control"
-            id="exampleInputEmail1"
-            aria-describedby="emailHelp"
-            placeholder="Enter email"
-          />
-          <small id="emailHelp" class="form-text text-muted">
-            We'll never share your email with anyone else.
-          </small>
-        </div>
-        <div class="form-group">
-          <label for="exampleInputPassword1">Password</label>
-          <input
-            type="password"
-            class="form-control"
-            id="exampleInputPassword1"
-            placeholder="Password"
-          />
-        </div>
-        <div class="form-check">
-          <input type="checkbox" class="form-check-input" id="exampleCheck1" />
-          <label class="form-check-label" for="exampleCheck1">
-            Keep me logged in
-          </label>
-        </div>
-        <button type="submit" className="btn w-100 mt-2 p-3 submit">
-          Submit
-        </button>
-      </form>
+    <div class="container" id="container">
+      <div class="form-container sign-in-container">
+        <form action="#">
+          <h1>Mindful Meals</h1>
+          <label>Email address</label>
+          <input type="email" placeholder="Email" />
+          <label>Pick a password</label>
+          <input type="password" placeholder="Password" />
+          <button>Submit</button>
+        </form>
+      </div>
     </div>
   );
 };
