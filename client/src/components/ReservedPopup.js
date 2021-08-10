c9e8c960534257e0d2a2790c5a87b433a4245e7d
merylschoice22 <kekrieg.digital@gmail.com>
2021-08-10 23:10:14 +0200

    Edited text in ReservedPopup.js

--- client/src/components/ReservedPopup.js
+++ client/src/components/ReservedPopup.js
@@ -6,24 +6,24 @@ function ReservedPopup(props) {
     <div className="popup">
       <div className="inner-popup">
         <div className="reserve-post-information">
-          <h3>We see that you are interested in this treat :)</h3>
+          <h3>We see that you are interested in this treat!</h3>
           <br></br>
           <div className="command-intro">
             <p>
-              You are one step away to reserve the food ! The above information
-              belongs to the Food Giver and we would like to ask you to contact
-              the person using the following :
+              You are one step away from reserving the food! The above
+              information belongs to the Food Giver and we would like to ask you
+              to contact the person using the following:
             </p>
             <br></br>
           </div>
           <div className="personal-details-post-owner">
             <div>
               <p className="location-food-giver">
-                Location is : Gracia, Carrer de Rabassa
+                Location is: Gracia, Carrer de Rabassa
               </p>
               <br></br>
               <p className="phone-food-giver">
-                The phone number is : 623 479 840
+                The phone number is: 623 479 840
               </p>
             </div>
           </div>
