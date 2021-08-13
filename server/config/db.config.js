7fa82f92c5922ba1f7b81f335fc9786499392376
merylschoice22 <kekrieg.digital@gmail.com>
2021-08-13 21:39:27 +0200

    Update to structure a bit

--- server/config/db.config.js
+++ server/config/db.config.js
@@ -1,14 +1,14 @@
 module.exports = {
-	HOST: "localhost",
-	USER: "postgres",
-	PASSWORD: "bruno",  // HEY!! dont forget to change according to your user. 
-	DB: "mindful_meals",
-	dialect: "postgres",
-	// POOL IS OPTIONAL :D ! is only for connections.
-	pool: {
-	  max: 5,
-	  min: 0,
-	  acquire: 30000,
-	  idle: 10000
-	}
-  };
+  HOST: "localhost",
+  USER: "postgres",
+  PASSWORD: "660182", // HEY!! dont forget to change according to your user.
+  DB: "mindful_meals",
+  dialect: "postgres",
+  // POOL IS OPTIONAL :D ! is only for connections.
+  pool: {
+    max: 5,
+    min: 0,
+    acquire: 30000,
+    idle: 10000,
+  },
+};
