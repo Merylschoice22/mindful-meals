f78718c6a9234c93b58c3259e6c4f69f10ef0cb4
merylschoice22 <kekrieg.digital@gmail.com>
2021-08-16 17:40:59 +0200

    Update structure for My Food Post and My Reserved Food

--- client/src/pages/MyFoodPosts.js
+++ client/src/pages/MyFoodPosts.js
@@ -1,9 +1,36 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import FoodCard from "../components/FoodCard";
 
 const MyFoodPosts = () => {
+  const [posts, setPosts] = useState([]);
+  useEffect(() => {
+    fetch("http://localhost:3000/myfoodposts/:userId")
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data);
+        setPosts(data);
+      })
+      .catch((error) => console.error(error));
+  }, []);
   return (
     <div>
-      <h1>MyFoodPosts</h1>
+      <h1>My Meal Posts</h1>
+      <h2>
+        Below are all of the meals which you have posted to share with others
+      </h2>
+      <h4>
+        You can keep track of the ones you have posted, and when someone is
+        interested, the meal card will be green and someone will contact you by
+        the phone number you have provided to coordinate where to collect it.
+        Once they have collected the food, please click on the "Collected"
+        button so that the meal transaction will be completed!
+      </h4>
+      {posts.map((postData, index) => (
+        <div className="___MYfoodcard" key={index}>
+          {/* Make a new version of this card to reflect what the food giver should see */}
+          <FoodCard postData={postData} />
+        </div>
+      ))}
     </div>
   );
 };
--- client/src/pages/MyReservedFood.js
+++ client/src/pages/MyReservedFood.js
@@ -1,9 +1,33 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import FoodCard from "../components/FoodCard";
 
 const MyReservedFood = () => {
+  const [posts, setPosts] = useState([]);
+  useEffect(() => {
+    fetch("http://localhost:3000/myfoodposts/:userId")
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data);
+        setPosts(data);
+      })
+      .catch((error) => console.error(error));
+  }, []);
   return (
     <div>
       <h1>My Reserved Food</h1>
+      <h2>Below are all of the meals which you have reserved!</h2>
+      <h4>
+        You can keep track of the meals you have reserved and easily find the
+        contact information here. Make sure to contact the person who is giving
+        the food away by the phone number they have provided to coordinate where
+        to collect it.
+      </h4>
+      {posts.map((postData, index) => (
+        <div className="___MYfoodcard" key={index}>
+          {/* Make a new version of this card to reflect what the food getter should see */}
+          <FoodCard postData={postData} />
+        </div>
+      ))}
     </div>
   );
 };
