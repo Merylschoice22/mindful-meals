63965062a07e7a14932bcc22d2698b39d67081af
merylschoice22 <kekrieg.digital@gmail.com>
2021-08-17 23:26:33 +0200

    Update fetch and text

--- client/src/pages/MyFoodPosts.js
+++ client/src/pages/MyFoodPosts.js
@@ -4,7 +4,7 @@ import FoodCardMyMealPost from "../components/FoodCardMyMealPost";
 const MyFoodPosts = () => {
   const [posts, setPosts] = useState([]);
   useEffect(() => {
-    fetch("http://localhost:3000/myfoodposts/:userId")
+    fetch("http://localhost:3000/mymealposts/:userId")
       .then((res) => res.json())
       .then((data) => {
         console.log(data);
@@ -16,25 +16,26 @@ const MyFoodPosts = () => {
     <div>
       <h1>My Meal Posts</h1>
       <h2>
-        Below are all of the meals which you have posted to share with others
+        Below are all of the meals and info which you have posted to share with
+        others
       </h2>
       <br></br>
-      <h3>
+      <h4>
         You can keep track of the ones you have posted, and when someone is
         interested, the meal card will be green and someone will contact you by
         the phone number you have provided to coordinate where to collect it.
-      </h3>
+      </h4>
       <br></br>
-      <h3>
+      <h4>
         Once they have collected the food, please click on the "Collected"
         button so that the meal transaction will be completed!
-      </h3>
+      </h4>
       <br></br>
-      <h3>
+      <h4>
         The person who reserved your meal has 30 minutes to contact you. If they
         haven't, then you can click the "Available" button again to make your
         meal visible for others to collect.
-      </h3>
+      </h4>
       {posts.map((postData, index) => (
         <div className="___MYfoodcard" key={index}>
           {/* Make a new version of this card to reflect what the food giver should see */}
