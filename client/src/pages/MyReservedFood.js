5dde93f166ad266607144b651a80f2d749ad041a
merylschoice22 <kekrieg.digital@gmail.com>
2021-08-17 23:26:08 +0200

    Update fetch and text

--- client/src/pages/MyReservedFood.js
+++ client/src/pages/MyReservedFood.js
@@ -4,7 +4,7 @@ import FoodCardMyReserved from "../components/FoodCardMyReserved";
 const MyReservedFood = () => {
   const [posts, setPosts] = useState([]);
   useEffect(() => {
-    fetch("http://localhost:3000/myfoodposts/:userId")
+    fetch("http://localhost:3000/myreservedposts/:userId")
       .then((res) => res.json())
       .then((data) => {
         console.log(data);
@@ -15,16 +15,21 @@ const MyReservedFood = () => {
   return (
     <div>
       <h1>My Reserved Food</h1>
-      <h2>Below are all of the meals which you have reserved!</h2>
+      <h2>Below are all of the meals which you have reserved!</h2> <br></br>
       <h4>
         You can keep track of the meals you have reserved and easily find the
-        contact information here. Make sure to contact the person who is giving
-        the food away by the phone number they have provided to coordinate where
-        to collect it.
+        details to collect your food. Make sure to contact the person who is
+        giving the food away by the phone number they have provided in order to
+        coordinate collecting it.
+      </h4>
+      <br></br>
+      <h4>
+        After reserving the food, you have 30 minutes to contact them, otherwise
+        they reserve the right to release the item back to the Food Feed so
+        others can reserve it.
       </h4>
       {posts.map((postData, index) => (
         <div className="___MYfoodcard" key={index}>
-          {/* Make a new version of this card to reflect what the food getter should see */}
           <FoodCardMyReserved postData={postData} />
         </div>
       ))}
