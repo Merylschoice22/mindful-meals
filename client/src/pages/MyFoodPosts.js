c01c299aba0561826f95dbf5b54ea5f5b61c6082
merylschoice22 <kekrieg.digital@gmail.com>
2021-08-18 09:12:55 +0200

    Fix styling for text

--- client/src/pages/MyFoodPosts.js
+++ client/src/pages/MyFoodPosts.js
@@ -17,28 +17,30 @@ const MyFoodPosts = () => {
     <div className="food-feed">
       <h1>My Meal Posts</h1>
       <Navbar />
-
-      <h2>
-        Below are all of the meals and info which you have posted to share with
-        others
-      </h2>
-      <br></br>
-      <h4>
-        You can keep track of the ones you have posted, and when someone is
-        interested, the meal card will be green and someone will contact you by
-        the phone number you have provided to coordinate where to collect it.
-      </h4>
-      <br></br>
-      <h4>
-        Once they have collected the food, please click on the "Collected"
-        button so that the meal transaction will be completed!
-      </h4>
-      <br></br>
-      <h4>
-        The person who reserved your meal has 30 minutes to contact you. If they
-        haven't, then you can click the "Available" button again to make your
-        meal visible for others to collect.
-      </h4>
+      <div className="food-feed">
+        <h2>
+          Below are all of the meals and info which you have posted to share
+          with others
+        </h2>
+        <br></br>
+        <h4>
+          You can keep track of the ones you have posted, and when someone is
+          interested, the meal card will be green and someone will contact you
+          by the phone number you have provided to coordinate where to collect
+          it.
+        </h4>
+        <br></br>
+        <h4>
+          Once they have collected the food, please click on the "Collected"
+          button so that the meal transaction will be completed!
+        </h4>
+        <br></br>
+        <h4>
+          The person who reserved your meal has 30 minutes to contact you. If
+          they haven't, then you can click the "Available" button again to make
+          your meal visible for others to collect.
+        </h4>
+      </div>
       {posts.map((postData, index) => (
         <div className="___MYfoodcard" key={index}>
           {/* Make a new version of this card to reflect what the food giver should see */}
--- client/src/pages/MyReservedFood.js
+++ client/src/pages/MyReservedFood.js
@@ -10,21 +10,23 @@ const MyReservedFood = () => {
     <div className="food-feed">
       <h1>My Reserved Food</h1>
       <Navbar />
-      <h2>Below are all of the meals which you have reserved!</h2> <br></br>
-      <h4>
-        You can keep track of the meals you have reserved and easily find the
-        details to collect your food. Make sure to contact the person who is
-        giving the food away by the phone number they have provided in order to
-        coordinate collecting it.
-      </h4>
-      <br></br>
-      <h4>
-        After reserving the food, you have 30 minutes to contact them, otherwise
-        they reserve the right to release the item back to the Food Feed so
-        others can reserve it.
-      </h4>
+      <div className="text">
+        <h2>Below are all of the meals which you have reserved!</h2> <br></br>
+        <h4>
+          You can keep track of the meals you have reserved and easily find the
+          details to collect your food. Make sure to contact the person who is
+          giving the food away by the phone number they have provided in order
+          to coordinate collecting it.
+        </h4>
+        <br></br>
+        <h4>
+          After reserving the food, you have 30 minutes to contact them,
+          otherwise they reserve the right to release the item back to the Food
+          Feed so others can reserve it.
+        </h4>
+      </div>
       <MyReservedSection />
-      <h3>Previous Meals</h3>
+      <h1 className="text">Previous Meals</h1>
       <MyCollectedSection />
     </div>
   );
