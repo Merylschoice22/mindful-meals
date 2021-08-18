029d59612b87f167fb5c7540f1e1f074cce72019
merylschoice22 <kekrieg.digital@gmail.com>
2021-08-18 04:48:49 +0200

    Create component for My Reserved section and move fetch for reserved posts, and food card into it

---
+++ client/src/components/MyReservedSection.js
@@ -0,0 +1,27 @@
+import React, { useState, useEffect } from "react";
+import FoodCardMyReserved from "./FoodCardMyReserved";
+
+const MyReservedSection = () => {
+  const [posts, setPosts] = useState([]);
+  useEffect(() => {
+    fetch("http://localhost:3000/myreservedposts/:userId")
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data);
+        setPosts(data);
+      })
+      .catch((error) => console.error(error));
+  }, []);
+
+  return (
+    <div>
+      {posts.map((postData, index) => (
+        <div className="___MYfoodcard" key={index}>
+          <FoodCardMyReserved postData={postData} />
+        </div>
+      ))}
+    </div>
+  );
+};
+
+export default MyReservedSection;
