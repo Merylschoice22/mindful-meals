c0470a31ca16ce67a051587ef8091b0fbd329e26
merylschoice22 <kekrieg.digital@gmail.com>
2021-08-18 10:41:20 +0200

    Add FoodPostForm functionality manually

--- client/src/components/FoodPostForm.js
+++ client/src/components/FoodPostForm.js
@@ -1,54 +1,122 @@
-import React from "react";
+import React, { useRef, useState, Component } from "react";
 import ShareFoodPost from "../buttons/ShareFoodPost";
+import { withRouter } from "react-router-dom";
 import "./FoodPostForm.css";
 
-function FoodPostForm() {
-  return (
-    <div className="food-post-form">
-      <div className="food-post-wrapper">
-        <p className="p-header">Fill out the form below to share your food</p>
-        <br></br>
-        <form>
-          <p className="post-label">Title</p>
-          <input
-            className="post-input"
-            type="text"
-            placeholder="Give your post a jazzy title. Ex: 'Delicious veggie salad for 2'"
-          ></input>
+class FoodPostForm extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      title: "",
+      loc_barrio: "",
+      loc_street: "",
+      phone: "",
+      description: "",
+    };
+
+    this.title = this.title.bind(this);
+    this.loc_barrio = this.loc_barrio.bind(this);
+    this.loc_street = this.loc_street.bind(this);
+    this.phone = this.phone.bind(this);
+    this.description = this.description.bind(this);
+    this.postfood = this.postfood.bind(this);
+  }
+
+  title(event) {
+    this.setState({ title: event.target.value });
+  }
+  loc_barrio(event) {
+    this.setState({ loc_barrio: event.target.value });
+  }
+  loc_street(event) {
+    this.setState({ loc_street: event.target.value });
+  }
+  phone(event) {
+    this.setState({ phone: event.target.value });
+  }
+  description(event) {
+    this.setState({ description: event.target.value });
+  }
+
+  postfood(event) {
+    event.preventDefault();
+    fetch("http://localhost:8080/new-post", {
+      //to specify what the backend expest to receive
+      headers: {
+        "Content-Type": "application/json",
+      },
+      method: "post",
+      body: JSON.stringify({
+        title: this.state.title,
+        loc_barrio: this.state.loc_barrio,
+        loc_street: this.state.loc_street,
+        phone: this.state.phone,
+        description: this.state.description,
+      }),
+    }).then((result) => {
+      if (result.status == 200) this.props.history.push("/");
+      else alert("Sorry!, Something went wrong!");
+    });
+  }
+
+  render() {
+    return (
+      <div className="food-post-form">
+        <div className="food-post-wrapper">
+          <p className="p-header">Fill out the form below to share your food</p>
           <br></br>
-          <p className="post-label">Neighborhood</p>
-          <input
-            className="post-input"
-            type="text"
-            placeholder="What neighborhood is the food in? Ex: 'Gracia'"
-          ></input>
-          <br></br>
-          <p className="post-label">Specific Street or Crossing</p>
-          <input
-            className="post-input"
-            type="text"
-            placeholder="Street / Cross streets / Any specific details. Ex: 'Arago con Bruc near the market'"
-          ></input>
-          <br></br>
-          <p className="post-label">Phone</p>
-          <input
-            className="post-input"
-            type="text"
-            placeholder="Enter your phone number. Ex: 655 555 555"
-          ></input>
-          <br></br>
-          <p className="post-label">Description</p>
-          <input
-            className="post-input"
-            type="text"
-            placeholder="Tell us about the food that you have. Ex: 'I made an extra large veggie salad and have enough to share for 2 people'"
-          ></input>
-          <br></br>
-          <ShareFoodPost />
-        </form>
+          <form onSubmit={this.postfood}>
+            <p className="post-label">Title</p>
+            <input
+              className="post-input"
+              type="text"
+              onChange={this.title}
+              placeholder="Give your post a jazzy title. Ex: 'Delicious veggie salad for 2'"
+              required
+            ></input>
+            <br></br>
+            <p className="post-label">Neighborhood</p>
+            <input
+              className="post-input"
+              type="text"
+              onChange={this.loc_barrio}
+              placeholder="What neighborhood is the food in? Ex: 'Gracia'"
+              required
+            ></input>
+            <br></br>
+            <p className="post-label">Specific Street or Crossing</p>
+            <input
+              className="post-input"
+              type="text"
+              onChange={this.loc_street}
+              placeholder="Street / Cross streets / Any specific details. Ex: 'Arago con Bruc near the market'"
+              required
+            ></input>
+            <p className="post-label">Phone</p>
+            <input
+              className="post-input"
+              type="text"
+              onChange={this.phone}
+              placeholder="Enter your phone number. Ex: 655 555 555"
+              required
+            ></input>
+            <br></br>
+            <p className="post-label">Description</p>
+            <input
+              className="post-input"
+              type="text"
+              onChange={this.description}
+              placeholder="Tell us about the food that you have. Ex: 'I made an extra large veggie salad and have enough to share for 2 people'"
+              required
+            ></input>
+            <br></br>
+            <ShareFoodPost />
+          </form>
+        </div>
       </div>
-    </div>
-  );
+    );
+  }
 }
 
-export default FoodPostForm;
+export default withRouter(FoodPostForm);
