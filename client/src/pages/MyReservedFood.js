06bafaadaa5b52a8775f41e919b5bf23491d2e79
merylschoice22 <kekrieg.digital@gmail.com>
2021-08-18 08:42:31 +0200

    Add Navbar and CSS

--- client/src/pages/MyReservedFood.js
+++ client/src/pages/MyReservedFood.js
@@ -2,11 +2,14 @@ import React from "react";
 // import FoodCardMyReserved from "../components/FoodCardMyReserved";
 import MyReservedSection from "../components/MyReservedSection";
 import MyCollectedSection from "../components/MyCollectedSection";
+import Navbar from "../components/navbar-links/Navbar";
+import "../pages/FoodFeed.css";
 
 const MyReservedFood = () => {
   return (
-    <div>
+    <div className="food-feed">
       <h1>My Reserved Food</h1>
+      <Navbar />
       <h2>Below are all of the meals which you have reserved!</h2> <br></br>
       <h4>
         You can keep track of the meals you have reserved and easily find the
