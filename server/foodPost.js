803cfbb8216091c439d8133d01ea6e43db1469e8
merylschoice22 <kekrieg.digital@gmail.com>
2021-08-18 16:13:46 +0200

    Delete FoodPost.js for now

--- server/foodPost.js
+++ server/foodPost.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 const bodyParser = require("body-parser");
+const relativeDate = require("tiny-relative-date");
 const { Pool } = require("pg");
 const secret = require("./secret.json");
 // const creator = require("./services/creator.js");
@@ -27,8 +28,7 @@ app.get("/all", (req, res) => {
 //Where status is available
 app.get("/", (req, res) => {
   const query =
-    // "SELECT u.username, p.id, p.user_id, p.title, p.loc_barrio, p.description, p.post_date, p.loc_street, p.phone FROM posts p INNER JOIN users u ON p.users_id = u.id WHERE p.status='available' order by p.post_date asc limit 5";
-    "SELECT * FROM posts";
+    "SELECT u.username, p.title, p.loc_barrio, p.description, p.post_date, p.loc_street, p.phone FROM posts p INNER JOIN users u ON p.users_id = u.id WHERE p.status='available' order by p.post_date asc limit 5";
   pool
     .query(query)
     .then((result) => {
