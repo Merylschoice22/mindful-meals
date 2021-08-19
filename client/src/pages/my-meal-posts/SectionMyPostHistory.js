82fb696bb5310077c2fd4408adf1d1580711c4ba
merylschoice22 <kekrieg.digital@gmail.com>
2021-08-20 01:45:57 +0200

    Create 2 section components for MyMealPosts, map postData to render food cards

---
+++ client/src/pages/my-meal-posts/SectionMyPostHistory.js
@@ -0,0 +1,20 @@
+import React from "react";
+import FoodCardCollected from "../../components/FoodCardCollected";
+
+const SectionMyPostHistory = ({ postsData }) => {
+  return (
+    <div>
+      {postsData.map((post) => {
+        return (
+          post.status === "collected" && (
+            <div className="___MYfoodcard" key={post.id}>
+              <FoodCardCollected postData={post} />
+            </div>
+          )
+        );
+      })}
+    </div>
+  );
+};
+
+export default SectionMyPostHistory;
