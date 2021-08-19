66a44243227bf84330ae86c039e9d666011d94c5
merylschoice22 <kekrieg.digital@gmail.com>
2021-08-20 01:32:03 +0200

    Pass setRefresh as a prop to FoodCardMyReserved

--- client/src/components/MyReservedSection.js
+++ client/src/components/MyReservedSection.js
@@ -1,14 +1,14 @@
 import React from "react";
 import FoodCardMyReserved from "./FoodCardMyReserved";
 
-const MyReservedSection = ({ postsData }) => {
+const MyReservedSection = ({ postsData, setRefresh }) => {
   return (
     <div>
       {postsData.map((post) => {
         return (
           post.status === "reserved" && (
             <div className="___MYfoodcard" key={post.id}>
-              <FoodCardMyReserved postData={post} />
+              <FoodCardMyReserved postData={post} setRefresh={setRefresh} />
             </div>
           )
         );
