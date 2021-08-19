fee3ea5d10fc91e7b76598635019693eee7c03e5
merylschoice22 <kekrieg.digital@gmail.com>
2021-08-20 01:17:49 +0200

    Update GET endpoint to only select status='available'

--- server/routes/foodfeed.routes.js
+++ server/routes/foodfeed.routes.js
@@ -5,7 +5,7 @@ module.exports = function (app, pool) {
       //This will be the real route
       // "SELECT u.username, p.title, p.loc_barrio, p.description, p.post_date, p.loc_street, p.phone FROM posts p INNER JOIN users u ON p.user_id = u.id WHERE p.status='available' order by p.post_date asc limit 5";
       // //This is to test out for now so we can see something until we link with users
-      "SELECT * FROM posts order by created_at desc";
+      "SELECT * FROM posts where status='available' order by created_at desc";
     pool
       .query(query)
       .then((result) => {
