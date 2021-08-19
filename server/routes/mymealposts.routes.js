2fb18cab514d8a9f8c98497ed4dc12a0fe76099a
merylschoice22 <kekrieg.digital@gmail.com>
2021-08-19 14:57:07 +0200

    Create route file for mymealposts and connect to server

---
+++ server/routes/mymealposts.routes.js
@@ -0,0 +1,15 @@
+module.exports = (app, pool) => {
+  //GET
+  //Validate user is signed in
+  app.get("/mymealposts/:userId", (req, res) => {
+    const userId = 3; //Get user ID
+    const query = `SELECT p.title, p.loc_barrio, p.loc_street, p.description, p.post_date, p.phone, p.status FROM posts p WHERE p.users_id=${userId} order by p.post_date desc`;
+    pool
+      .query(query)
+      .then((result) => {
+        //Validate user is signed in
+        res.json(result.rows);
+      })
+      .catch((error) => console.error(error));
+  });
+};
