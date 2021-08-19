162c36f786d410d97b927242022e7481fa5c1a59
merylschoice22 <kekrieg.digital@gmail.com>
2021-08-19 18:23:03 +0200

    Update routes so that it is one get for the whole page and the front end filters by section

--- server/routes/myreservedfood.routes.js
+++ server/routes/myreservedfood.routes.js
@@ -1,32 +1,13 @@
 module.exports = (app, pool) => {
-  //GET all food posts that I have reserved
-  //By my user ID
-  //Validate user is signed in
   app.get("/myreservedposts/:userId", (req, res) => {
-    const userId = 3; //Get user ID currently logged in
-    const query = `SELECT p.title, p.loc_barrio, p.loc_street, p.description, p.post_date, p.phone, p.status FROM posts p WHERE p.user_id=${userId} and status='reserved' order by p.post_date asc`;
+    const userId = 3;
+    const query = `SELECT p.id, p.title, p.loc_barrio, p.loc_street, p.description, p.post_date, p.phone, p.status FROM posts p WHERE p.user_id=${userId} order by p.post_date asc`;
 
     pool
       .query(query)
       .then((result) => {
         //Validate user is signed in
-        res.json(result.rows);
-      })
-      .catch((error) => console.error(error));
-  });
 
-  //GET History
-  //Previous food that I have collected
-  //By my user ID
-  //Validate user is signed in
-  app.get("/mycollectedposts/:userId", (req, res) => {
-    const userId = 3; //Get user ID
-    const query = `SELECT p.title, p.loc_barrio, p.loc_street, p.description, p.post_date, p.phone, p.status FROM posts p WHERE p.user_id=${userId} and status='collected' order by p.post_date asc`;
-
-    pool
-      .query(query)
-      .then((result) => {
-        //Validate user is signed in
         res.json(result.rows);
       })
       .catch((error) => console.error(error));
@@ -34,7 +15,8 @@ module.exports = (app, pool) => {
 
   //PATCH - Cancel a reservation - Update status from reserved to available
   app.patch("/status-available/:postId", (req, res) => {
-    const postId = 2; //Get postID from front
+    const postId = req.params.postId; //Get postID from front
+    console.log(postId);
     const queryUPDATE = "UPDATE posts SET status=$1 WHERE id=$2";
     pool
       .query(queryUPDATE, ["available", postId])
