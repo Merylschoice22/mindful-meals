1cd13e6ae26ed793b2cb7a588005aa7f9e8e9944
merylschoice22 <kekrieg.digital@gmail.com>
2021-08-19 18:49:02 +0200

    Remove fetch and change props that are passed in

--- client/src/buttons/BtnCancelReservation.js
+++ client/src/buttons/BtnCancelReservation.js
@@ -1,36 +1,10 @@
-import React, { useEffect } from "react";
+import React from "react";
 import "../components/FoodCardMyReserved.css";
 
 const BtnCancelReservation = ({ handleClick }) => {
-  //   const statusAvailable = () => {
-  //     setStatus("available");
-  //   };
-  const handleStatus = () => {
-    handleClick("available");
-  };
-  useEffect(() => {
-    fetch("http://localhost:3000/status-available/:postId", {
-      method: "PATCH",
-      body: JSON.stringify({
-        completed: true,
-      }),
-      headers: {
-        "Content-type": "application/json; charset=UTF-8",
-      },
-    })
-      .then((res) => {
-        res.json();
-      })
-      .then((data) => {
-        console.log(data);
-        // handleStatus;
-      })
-      .catch((error) => console.error(error));
-  }, []);
-
   return (
     <div className="going-button">
-      <button className="going-post-food-btn" onClick={() => handleStatus()}>
+      <button className="going-post-food-btn" onClick={handleClick}>
         Cancel Reservation
       </button>
     </div>
