813fb010935b75ed81851fe87d02e4b26cbec1ef
merylschoice22 <kekrieg.digital@gmail.com>
2021-08-21 17:07:21 +0200

    Import authJwt middleware and add it to GET route and patch route. Add userId variable to get the userId from what is sent back from front end and add it to query specifying GET where user_id = currently logged in user. Add userId to PATCH query to specify updating posts whose user_id is the current userId.

--- server/routes/mymealposts.routes.js
+++ server/routes/mymealposts.routes.js
@@ -1,8 +1,10 @@
+const { authJwt } = require("../middleware");
+
 module.exports = (app, pool) => {
   //GET my food posts that I have done based on my signed in ID
   //Validate user is signed in
-  app.get("/mymealposts/:userId", (req, res) => {
-    const userId = 1; //Get user ID
+  app.get("/mymealposts/", authJwt.verifyToken, (req, res) => {
+    const userId = req.userId; //Get user ID
     const query = `SELECT p.id, p.title, p.loc_barrio, p.loc_street, p.description, p.post_date, p.phone, p.status FROM posts p WHERE p.user_id=${userId} order by p.post_date desc`;
     pool
       .query(query)
@@ -14,11 +16,11 @@ module.exports = (app, pool) => {
   });
 
   //PATCH - Mark collected - Update status from reserved to collected
-  app.patch("/status-collected/:postId", (req, res) => {
+  app.patch("/status-collected/:postId", authJwt.verifyToken, (req, res) => {
     const postId = req.params.postId;
-    const query = "UPDATE posts SET status=$1 WHERE id=$2";
+    const query = "UPDATE posts SET status=$1 WHERE id=$2 and user_id=$3";
     pool
-      .query(query, ["collected", postId])
+      .query(query, ["collected", postId, req.userId])
       .then(() => {
         res.status(200).send("Food successfully collected!");
       })
