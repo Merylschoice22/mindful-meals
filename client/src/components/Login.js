81a082176c224c61f79333ba40400d090565e1dc
merylschoice22 <kekrieg.digital@gmail.com>
2021-08-21 04:15:49 +0200

    Add prompt and link to Register from Login page and style

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import { Link } from "react-router-dom";
 import "./Login.css";
 
 export default class Login extends Component {
@@ -61,6 +62,10 @@ export default class Login extends Component {
               placeholder="Password"
             />
             <button>Submit</button>
+            <div>
+              <p>Don't have an account?</p>
+              <Link to="/register">Register here!</Link>
+            </div>
           </form>
         </div>
       </div>
