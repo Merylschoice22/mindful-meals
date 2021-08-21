be5b3fd1f93e2461fb39683efb9a696edf895e57
merylschoice22 <kekrieg.digital@gmail.com>
2021-08-21 04:39:26 +0200

    Update error message

--- server/controllers/auth.controller.js
+++ server/controllers/auth.controller.js
@@ -58,7 +58,7 @@ exports.signin = (req, res) => {
   })
     .then((user) => {
       if (!user) {
-        return res.status(404).send({ message: "User Not found." });
+        return res.status(404).send({ message: "User not found." });
       }
 
       let passwordIsValid = bcrypt.compareSync(
