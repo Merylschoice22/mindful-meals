bb006495fba48af88a9bf62b5b9bd9be5f0ccb40
merylschoice22 <kekrieg.digital@gmail.com>
2021-08-22 02:22:43 +0200

    Update GET query to include username, join table, and order by created_at desc. Modify patch route for making a reserved status available again to differentiate from mymealposts.routes

--- server/routes/myreservedfood.routes.js
+++ server/routes/myreservedfood.routes.js
@@ -3,25 +3,24 @@ const { authJwt } = require("../middleware"); //check
 module.exports = (app, pool) => {
   app.get("/myreservedposts", authJwt.verifyToken, (req, res) => {
     const userId = req.userId;
-    const query = `SELECT p.id, p.title, p.loc_barrio, p.loc_street, p.description, p.post_date, p.phone, p.status FROM posts p WHERE p.reserved_by_user_id=$1 order by p.post_date asc`;
+    const query = `SELECT u.username, p.id, p.title, p.loc_barrio, p.loc_street, p.description, p.post_date, p.phone, p.status FROM posts p INNER JOIN users u ON p.user_id = u.id WHERE p.reserved_by_user_id=$1 order by p.created_at desc`;
 
     pool
-      .query(query)
+      .query(query, [userId])
       .then((result) => {
-        //Validate user is signed in
-
         res.json(result.rows);
       })
       .catch((error) => console.error(error));
   });
 
   //PATCH - Cancel a reservation - Update status from reserved to available
-  app.patch("/status-available/:postId", authJwt.verifyToken, (req, res) => {
-    const postId = req.params.postId; //Get postID from front
-    console.log(postId);
-    const queryUPDATE = "UPDATE posts SET status=$1 WHERE id=$2 and user_id=$3";
+  app.patch("/status-cancel-res/:postId", authJwt.verifyToken, (req, res) => {
+    const userId = req.userId;
+    const postId = req.params.postId;
+    const queryUPDATE =
+      "UPDATE posts SET status=$1 WHERE id=$2 and reserved_by_user_id=$3";
     pool
-      .query(queryUPDATE, ["available", postId, req.userId])
+      .query(queryUPDATE, ["available", postId, userId])
       .then(() => {
         res.status(200).send("Food successfully unreserved!");
       })
