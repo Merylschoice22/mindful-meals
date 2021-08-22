8e1fba3fd6145645ab286b92c68b5ad106a25486
merylschoice22 <kekrieg.digital@gmail.com>
2021-08-22 23:22:48 +0200

    Update Logout page and CSS

--- client/src/pages/Logout.js
+++ client/src/pages/Logout.js
@@ -1,6 +1,5 @@
 import React from "react";
 import { Link, useHistory } from "react-router-dom";
-import "../components/FoodPostForm.css";
 import "../components/Login.css";
 
 const Logout = () => {
@@ -10,19 +9,20 @@ const Logout = () => {
     history.push("/login");
   };
   return (
-    <div id="container">
-      <div className="food-post-wrapper ">
-        <div className="food-post-form ">
-          <div className=".form-container ">
-            <h1>Are you sure you want to leave?</h1>
-            <button className="share-btn" onClick={logout}>
-              Log out
-            </button>
-            <h3 className="p-header">
-              <Link to="/login">Log in</Link> as a different user
-            </h3>
+    <div className="container" id="container">
+      <div className="form-container sign-in-container">
+        <form onSubmit={this.login}>
+          <h1>Are you sure you want to leave?</h1>
+          <button className="share-btn" onClick={logout}>
+            Log out
+          </button>
+          <br></br>
+          <div>
+            <h2>
+              <Link to="/login">LOG IN</Link> as a different user
+            </h2>
           </div>
-        </div>
+        </form>
       </div>
     </div>
   );
