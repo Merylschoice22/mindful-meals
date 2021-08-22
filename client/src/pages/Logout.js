5f89784e75df6ca3f58420f00e241a186de720a5
merylschoice22 <kekrieg.digital@gmail.com>
2021-08-22 04:23:06 +0200

    Redirect Logout to Login after clicking button

--- client/src/pages/Logout.js
+++ client/src/pages/Logout.js
@@ -1,12 +1,13 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import { Link, useHistory } from "react-router-dom";
 import "../components/FoodPostForm.css";
 import "../components/Login.css";
 
 const Logout = () => {
-  //   const accessToken = localStorage.getItem("accessToken");
+  let history = useHistory();
   const logout = () => {
     localStorage.removeItem("accessToken");
+    history.push("/login");
   };
   return (
     <div id="container">
