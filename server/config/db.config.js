b2a82a1ab3c2c7e677421d45faab7604e1a4eb04
merylschoice22 <kekrieg.digital@gmail.com>
2021-08-24 17:43:29 +0200

    Install pg-connection-string and config

--- server/config/db.config.js
+++ server/config/db.config.js
@@ -1,10 +1,15 @@
+const parse = require("pg-connection-string").parse;
+
+const config = parse(process.env.DATABASE_URL);
+console.log("Postgres connection info", config);
+
 module.exports = {
-  HOST: "localhost",
-  USER: "postgres",
-  PASSWORD: "", // HEY!! dont forget to change according to your user.
-  DB: "mindful_meals",
+  HOST: config.host,
+  USER: config.user,
+  PASSWORD: config.password, // HEY!! dont forget to change according to your user.
+  DB: config.database,
   dialect: "postgres",
-  PORT: 5432,
+  PORT: config.port,
   // POOL IS OPTIONAL :D ! is only for connections.
   pool: {
     max: 5,
